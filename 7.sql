2c5388f2c599038f627e38c95750eb23e9a597d5
jonathanhinton <hintonjonathan@gmail.com>
2016-01-13 21:04:31 -0600

    remove parenthesis

--- 7.sql
+++ 7.sql
@@ -5,4 +5,4 @@ SELECT
 FROM
   Customer AS C JOIN Invoice AS I JOIN Employee AS E
 ON
-  (C.CustomerId == I.CustomerId) AND C.SupportRepId == E.EmployeeId
+  C.CustomerId == I.CustomerId AND C.SupportRepId == E.EmployeeId
