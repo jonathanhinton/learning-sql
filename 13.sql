d72b304276792476a867c4009f3ea49dc318a66d
jonathanhinton <hintonjonathan@gmail.com>
2016-01-18 17:52:37 -0600

    join tables to display artist instead of composer

--- 13.sql
+++ 13.sql
@@ -1,7 +1,11 @@
 SELECT
-  IL.InvoiceLineId, IL.InvoiceId, IL.TrackId, IL.UnitPrice, IL.Quantity, T.Name, T.Composer
+  IL.InvoiceLineId, IL.InvoiceId, IL.TrackId, IL.UnitPrice, IL.Quantity, T.Name, Artist.Name AS Artist
 FROM
   InvoiceLine AS IL JOIN Track AS T
 ON
   IL.TrackId == T.TrackId
+JOIN Album ON
+  T.AlbumId == Album.AlbumId
+JOIN Artist ON
+  Album.ArtistId == Artist.ArtistId
 GROUP BY IL.InvoiceLineId
