676e998407276ff5638119d4e6a49fcf64434959
jonathanhinton <hintonjonathan@gmail.com>
2016-01-18 17:23:03 -0600

    remove extra join

--- 12.sql
+++ 12.sql
@@ -1,7 +1,7 @@
 SELECT
   IL.InvoiceLineId, IL.InvoiceId, IL.TrackId, IL.UnitPrice, IL.Quantity, T.Name
 FROM
-  Customer AS C JOIN InvoiceLine AS IL JOIN Track AS T
+  JOIN InvoiceLine AS IL JOIN Track AS T
 ON
   IL.TrackId == T.TrackId
 GROUP BY IL.InvoiceLineId
